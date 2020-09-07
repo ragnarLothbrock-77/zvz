c28ad59f798b3b33a5a41733b693c10f2ba118fa
Ragnar <semen.miroshnichenko.07@mail.ru>
2020-09-07 21:16:27 +0300

    Updates

---
+++ precache-manifest.b389b3bdcd2ee6da80e6d36fe4fbd24a.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe70cb211af05c55a4e24c1458d8b481",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3dbb01f518b8ba4a7289",
+    "url": "/static/css/main.975d2fb5.chunk.css"
+  },
+  {
+    "revision": "8d0b286f3717c79b671e",
+    "url": "/static/js/2.1c47220f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1c47220f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dbb01f518b8ba4a7289",
+    "url": "/static/js/main.7f3f886d.chunk.js"
+  },
+  {
+    "revision": "a761aa0d8f227c131a45",
+    "url": "/static/js/runtime-main.4bb3de27.js"
+  },
+  {
+    "revision": "8ba765ce9580a3bdae76b59130d0ef8a",
+    "url": "/static/media/AmaticSC-Bold.8ba765ce.ttf"
+  },
+  {
+    "revision": "e9a42e339f93a719c17be53c386dbbfb",
+    "url": "/static/media/AmaticSC-Regular.e9a42e33.ttf"
+  },
+  {
+    "revision": "3001cff3181eefd8db327cd724309a09",
+    "url": "/static/media/Caveat-Bold.3001cff3.ttf"
+  },
+  {
+    "revision": "b20ff458bc0dc48c039c68a3dc96173c",
+    "url": "/static/media/Caveat-Regular.b20ff458.ttf"
+  },
+  {
+    "revision": "c81f697f4bb9340bbc60fd1185d67509",
+    "url": "/static/media/CormorantInfant-Bold.c81f697f.ttf"
+  },
+  {
+    "revision": "0286a9a2112f231ba5858a50b9e994c4",
+    "url": "/static/media/CormorantInfant-BoldItalic.0286a9a2.ttf"
+  },
+  {
+    "revision": "3958a1ff7c7eaabc0e958b0374d71c61",
+    "url": "/static/media/CormorantInfant-Light.3958a1ff.ttf"
+  },
+  {
+    "revision": "6387d56067ae4144d24151132d4be4b2",
+    "url": "/static/media/CormorantInfant-LightItalic.6387d560.ttf"
+  },
+  {
+    "revision": "2a2fb473ab6ce3b5ab8e6bb4233ad4cf",
+    "url": "/static/media/CormorantInfant-Medium.2a2fb473.ttf"
+  },
+  {
+    "revision": "c09ad97da5315932410ef90130b81d5e",
+    "url": "/static/media/CormorantInfant-MediumItalic.c09ad97d.ttf"
+  },
+  {
+    "revision": "ca1084f3b02e6dd1c468b870f398bea9",
+    "url": "/static/media/CormorantInfant-Regular.ca1084f3.ttf"
+  },
+  {
+    "revision": "27e3649bab7c62fa21b8837c4842e40e",
+    "url": "/static/media/Montserrat-Black.27e3649b.ttf"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "409c7f79a42e56c785f50ed37535f0be",
+    "url": "/static/media/Montserrat-Light.409c7f79.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "43dd5b7a3d277362d5e801e5353e3a01",
+    "url": "/static/media/Montserrat-Thin.43dd5b7a.ttf"
+  },
+  {
+    "revision": "1c0ffc625ada48fd543b08fdc959b032",
+    "url": "/static/media/clubs-bg.1c0ffc62.jpg"
+  },
+  {
+    "revision": "d2e0ec048a914f01c07dd7344728a08a",
+    "url": "/static/media/helena.d2e0ec04.jpg"
+  },
+  {
+    "revision": "82aa21192506f720182ec283e311bad0",
+    "url": "/static/media/items-bg.82aa2119.jpg"
+  },
+  {
+    "revision": "a304b845fc6353b99e9aeae174ccc492",
+    "url": "/static/media/logo.a304b845.png"
+  },
+  {
+    "revision": "cc64fb766aefe6e0267edfec6c283018",
+    "url": "/static/media/main-bg.cc64fb76.png"
+  },
+  {
+    "revision": "80d724b0268ce7107c148b7989c2a65d",
+    "url": "/static/media/semen.80d724b0.jpeg"
+  },
+  {
+    "revision": "81ca10c30b76077a240cd50e7c33b444",
+    "url": "/static/media/services-bg.81ca10c3.jpg"
+  },
+  {
+    "revision": "c50defe101a18d11b61becef41da0b7b",
+    "url": "/static/media/template.c50defe1.jpg"
+  },
+  {
+    "revision": "704312af72af7527761e339491c00da0",
+    "url": "/static/media/zvz.704312af.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/zvz/precache-manifest.9dc51f50b4e01d6029c693420bd1b65b.js"
+  "/precache-manifest.b389b3bdcd2ee6da80e6d36fe4fbd24a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/zvz/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
